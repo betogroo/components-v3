f951d274039d056414bd76d06bddc5647afbd5ef
betogroo <luizhumberto@gmail.com>
2023-06-06 16:34:30 -0300

    refactor: separate routes file

--- src/plugins/vue-router.ts
+++ src/plugins/vue-router.ts
@@ -1,22 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'HomeView',
-    component: () => import('@/views/HomeView.vue')
-  },
-  {
-    path: '/about',
-    name: 'AboutView',
-    component: () => import('@/views/AboutView.vue')
-  },
-  {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: () => import('@/views/NotFound.vue')
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from '@/router'
 
 const router = createRouter({
   routes,
