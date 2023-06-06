92a9b714d642ae05b849e3a59f7146c58ddc5284
betogroo <luizhumberto@gmail.com>
2023-06-06 17:07:52 -0300

    feat: create CounterView (options api) and router

---
+++ src/views/CounterView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height text-center">
+    <v-responsive>
+      <h1>Counter</h1>
+      <h2>{{ counter }}</h2>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      counter: 1
+    }
+  }
+})
+</script>
