df0ae17bebd269789c0a2341b5cc3c36db06700d
betogroo <luizhumberto@gmail.com>
2023-06-07 15:56:18 -0300

    refactor: create CounterView Component

---
+++ src/components/CounterView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import useCounter from '@/composables/useCounter'
+
+const { counter, increaseValue, decreaseValue, resetCounter } = useCounter(
+  Number(10)
+)
+</script>
+
+<template>
+  <v-card width="400" class="pa-3">
+    <h1 class="text-h1 text-center">{{ counter }}</h1>
+    <div class="mb-4 d-flex justify-space-between">
+      <v-btn
+        :disabled="counter === 0"
+        min-width="174"
+        @click="decreaseValue"
+        color="red"
+        ><v-icon>mdi-minus-thick</v-icon></v-btn
+      >
+      <v-btn min-width="174" @click="increaseValue" color="blue"
+        ><v-icon>mdi-plus-thick</v-icon></v-btn
+      >
+    </div>
+    <v-btn :disabled="counter === 0" @click="resetCounter" color="error" block
+      >Reset</v-btn
+    >
+  </v-card>
+</template>
--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,28 +1,10 @@
 <script setup lang="ts">
-import useCounter from '@/composables/useCounter'
-
-const { counter, increaseValue, decreaseValue, resetCounter } = useCounter(10)
+import CounterView from '@/components/CounterView.vue'
 </script>
 
 <template>
   <v-container class="fill-height d-flex justify-center">
-    <v-card width="400" class="text-center pa-3">
-      <h1 class="text-h1">{{ counter }}</h1>
-      <div class="mb-4 d-flex justify-space-between">
-        <v-btn
-          :disabled="counter === 0"
-          min-width="174"
-          @click="decreaseValue"
-          color="red"
-          ><v-icon>mdi-minus-thick</v-icon></v-btn
-        >
-        <v-btn min-width="174" @click="increaseValue" color="blue"
-          ><v-icon>mdi-plus-thick</v-icon></v-btn
-        >
-      </div>
-      <v-btn :disabled="counter === 0" @click="resetCounter" color="error" block
-        >Reset</v-btn
-      >
-    </v-card>
+    <CounterView />
+    <CounterView />
   </v-container>
 </template>
