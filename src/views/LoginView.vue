36dc55f45f0fecc68578e84bd5695f068b98dc47
Luiz Humberto <luizhumberto@gmail.com>
2023-06-09 14:12:13 -0300

    feat: create login form and route

---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container class="fill-height justify-center">
+    <v-card elevation="6" class="pa-4 rounded" width="400">
+      <v-card-title class="text-center text-h4 mb-4">Login</v-card-title>
+      <v-form>
+        <v-text-field
+          hint="Digite o seu email de cadastro"
+          type="email"
+          label="Email"
+          variant="outlined"
+        />
+        <v-text-field
+          hint="A senha deve conter números e letras"
+          type="password"
+          label="Senha"
+          variant="outlined"
+        />
+        <v-btn color="primary" block>Login</v-btn>
+      </v-form>
+      <p class="mt-3 text-subtitle text-body-1">
+        Esqueceu a senha? Clique
+        <router-link :to="{ name: 'HomeView' }">aqui</router-link> para
+        restaurar
+      </p>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
