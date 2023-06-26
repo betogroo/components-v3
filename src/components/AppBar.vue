d26e212e5037099d764f1fde56d5314942bc5f98
betogroo <luizhumberto@gmail.com>
2023-06-26 11:02:30 -0300

    feat: add login and signup lint in app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -5,6 +5,8 @@
       <v-btn :to="{ name: 'HomeView' }">Home</v-btn>
       <v-btn :to="{ name: 'CounterView' }">Counter</v-btn>
       <v-btn :to="{ name: 'AboutView' }">About</v-btn>
+      <v-btn :to="{ name: 'LoginView' }">Login</v-btn>
+      <v-btn :to="{ name: 'SignupView' }">Signup</v-btn>
     </div>
   </v-app-bar>
 </template>
