8d3f43342c271c1cab9db32ae5dd16440dcff863
Luiz Humberto <luizhumberto@gmail.com>
2023-06-27 16:23:31 -0300

    fix: appbar conditions and useAuth

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -9,16 +9,16 @@ const handleClick = async () => {
   <v-app-bar color="primary">
     <v-app-bar-title>Components</v-app-bar-title>
     <div>
-      <v-btn :to="{ name: 'HomeView' }">Home</v-btn>
-      <v-btn :to="{ name: 'CounterView' }">Counter</v-btn>
-      <v-btn :to="{ name: 'AboutView' }">About</v-btn>
+      <template v-if="user">
+        <v-btn :to="{ name: 'HomeView' }">Home</v-btn>
+        <v-btn :to="{ name: 'CounterView' }">Counter</v-btn>
+        <v-btn :to="{ name: 'AboutView' }">About</v-btn>
+        {{ user.email }}
+        <v-btn @click="handleClick">Logout</v-btn>
+      </template>
       <template v-if="!user">
         <v-btn :to="{ name: 'LoginView' }">Login</v-btn>
         <v-btn :to="{ name: 'SignupView' }">Signup</v-btn>
-      </template>
-      <template v-else>
-        {{ user.email }}
-        <v-btn @click="handleClick">Logout</v-btn>
       </template>
     </div>
   </v-app-bar>
--- src/modules/auth/composables/useAuth.ts
+++ src/modules/auth/composables/useAuth.ts
@@ -8,57 +8,57 @@ import {
   onAuthStateChanged,
 } from '@/plugins/firebase'
 
+const email = ref('')
+const password = ref('')
+const error = ref<string | null>(null)
+const isPending = ref(false)
+const user = ref(auth.currentUser)
+
+onAuthStateChanged(auth, (_user) => {
+  console.log(`User state changed. Current user is ${JSON.stringify(_user)}`)
+  user.value = _user
+})
+
+const login = async () => {
+  error.value = null
+  isPending.value = true
+  try {
+    const res = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value,
+    )
+    if (!res) {
+      throw new FirebaseError('auth/default-error', 'Erro ao entrar')
+    }
+    error.value = null
+    isPending.value = false
+  } catch (err) {
+    const e = err as FirebaseError
+    console.log(e.code)
+    error.value = e.message
+    isPending.value = false
+  }
+}
+
+const signup = async (email: string, password: string) => {
+  error.value = null
+  isPending.value = true
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    if (!res) {
+      throw new FirebaseError('auth/default-error', 'Erro ao cadastrar')
+    }
+    error.value = null
+    isPending.value = false
+  } catch (err) {
+    const e = err as FirebaseError
+    console.log(e.code)
+    error.value = e.message
+    isPending.value = false
+  }
+}
 const useAuth = () => {
-  const email = ref('')
-  const password = ref('')
-  const error = ref<string | null>(null)
-  const isPending = ref(false)
-  const user = ref(auth.currentUser)
-
-  onAuthStateChanged(auth, (_user) => {
-    console.log(`User state changed. Current user is ${JSON.stringify(_user)}`)
-    user.value = _user
-  })
-
-  const login = async () => {
-    error.value = null
-    isPending.value = true
-    try {
-      const res = await signInWithEmailAndPassword(
-        auth,
-        email.value,
-        password.value,
-      )
-      if (!res) {
-        throw new FirebaseError('auth/default-error', 'Erro ao entrar')
-      }
-      error.value = null
-      isPending.value = false
-    } catch (err) {
-      const e = err as FirebaseError
-      console.log(e.code)
-      error.value = e.message
-      isPending.value = false
-    }
-  }
-
-  const signup = async (email: string, password: string) => {
-    error.value = null
-    isPending.value = true
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password)
-      if (!res) {
-        throw new FirebaseError('auth/default-error', 'Erro ao cadastrar')
-      }
-      error.value = null
-      isPending.value = false
-    } catch (err) {
-      const e = err as FirebaseError
-      console.log(e.code)
-      error.value = e.message
-      isPending.value = false
-    }
-  }
   return {
     auth,
     email,
