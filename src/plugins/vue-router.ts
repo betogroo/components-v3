cfa67e8c65a36a7dcb118debf21a45d6fc83135d
betogroo <luizhumberto@gmail.com>
2023-06-28 14:37:25 -0300

    chore: create beforeEach router guard

--- src/plugins/vue-router.ts
+++ src/plugins/vue-router.ts
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 import routes from '@/router/routes'
+
+const requireAuth = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
+  console.log('to: ', to.name, 'from: ', from.name)
+  next()
+}
+
 const router = createRouter({
   routes,
   history: createWebHistory(process.env.BASE_URL),
 })
+router.beforeEach(requireAuth)
 
 export default router
