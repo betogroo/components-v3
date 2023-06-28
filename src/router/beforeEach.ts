43c3571c0fa6e2be44df58d879469618d436b399
betogroo <luizhumberto@gmail.com>
2023-06-28 15:46:24 -0300

    refactor: move auth route guard logic to beforeEach file

--- src/router/beforeEach.ts
+++ src/router/beforeEach.ts
@@ -1,6 +1,26 @@
 import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
+import { auth } from '@/plugins/firebase'
 
 export default (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
+  const user = auth.currentUser
+  if (to.name === 'LoginView' || to.name === 'SignupView') {
+    console.log('to=>', to.name, ' from=>', from.name)
+    next()
+  } else {
+    if (!user) {
+      next({ name: 'LoginView' })
+    } else {
+      console.log('Logado')
+      next()
+    }
+  }
+}
+
+/* export default (
   to: RouteLocationNormalized,
   from: RouteLocationNormalized,
   next: NavigationGuardNext,
@@ -8,3 +28,4 @@ export default (
   console.log('to=>', to.name, ' from=>', from.name)
   next()
 }
+ */
