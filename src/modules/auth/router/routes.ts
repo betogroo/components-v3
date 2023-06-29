7a5632649ee7b71cdc79f5c44cf1bbad17992b52
betogroo <luizhumberto@gmail.com>
2023-06-29 11:48:59 -0300

    chore: add required field meta in routes

--- src/modules/auth/router/routes.ts
+++ src/modules/auth/router/routes.ts
@@ -1,14 +1,20 @@
-import { RouteRecordRaw } from 'vue-router'
-const routes: RouteRecordRaw[] = [
+import { CustomRouteRecordRaw } from 'vue-router'
+const routes: CustomRouteRecordRaw[] = [
   {
     path: '/login',
     name: 'LoginView',
     component: () => import('../views/LoginView.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: '/signup',
     name: 'SignupView',
     component: () => import('../views/SignupView.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
 ]
 
--- src/modules/main/router/routes.ts
+++ src/modules/main/router/routes.ts
@@ -1,20 +1,30 @@
-import { RouteRecordRaw } from 'vue-router'
+import { CustomRouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
+const routes: CustomRouteRecordRaw[] = [
   {
     path: '/',
     name: 'HomeView',
     component: () => import('../views/HomeView.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: '/about',
     name: 'AboutView',
     component: () => import('../views/AboutView.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
+
   {
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: () => import('../views/NotFound.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
 ]
 
