79ae0732b79fa990aa6d62225290787019c94b6c
betogroo <luizhumberto@gmail.com>
2023-07-04 08:30:36 -0300

    fix: type DocumentData

--- src/modules/purchase/composables/usePurchase.ts
+++ src/modules/purchase/composables/usePurchase.ts
@@ -1,14 +1,19 @@
-import { db, getDocs, collection, addDoc } from '@/plugins/firebase'
+import {
+  db,
+  getDocs,
+  collection,
+  addDoc,
+  DocumentData,
+} from '@/plugins/firebase'
 import { ref } from 'vue'
 import type { Purchase } from '../model'
 const purchases = ref<Purchase[] | null>([])
 
 const collectionReference = collection(db, 'buy')
 getDocs(collectionReference).then((snapshot) => {
-  const docs: any[] = []
-  snapshot.docs.forEach((doc) => {
+  const docs: Purchase[] = []
+  snapshot.docs.forEach((doc: DocumentData) => {
     docs.push({ ...doc.data(), id: doc.id })
-    //console.log(doc.data().innerProcess)
   })
   purchases.value = docs
 })
