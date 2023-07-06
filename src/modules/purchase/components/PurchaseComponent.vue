d25766f089db0774682cd3067a7ec65da8f13779
Luiz Humberto <luizhumberto@gmail.com>
2023-07-06 17:17:32 -0300

    chore: create timestampToYear function

--- src/modules/purchase/components/PurchaseComponent.vue
+++ src/modules/purchase/components/PurchaseComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import getDocument from '@/shared/composables/getDocument'
 import type { Purchase } from '../model'
+import { Timestamp } from 'firebase/firestore'
 const props = defineProps<Props>()
 const { document: purchase, error } = await getDocument<Purchase>(
   'buy',
@@ -9,13 +10,22 @@ const { document: purchase, error } = await getDocument<Purchase>(
 interface Props {
   id: string
 }
+
+const timestampToYear = (date: Timestamp) => {
+  const seconds = date.seconds * 1000
+  const year = new Date(seconds).getFullYear()
+  return year
+}
 </script>
 
 <template>
-  <v-card v-if="purchase">
-    <v-card-title>Processo {{ purchase.innerProcess }}</v-card-title>
-    <v-card-text> {{ purchase.description }} </v-card-text>
-  </v-card>
+  <div>
+    <v-row v-if="purchase">
+      <v-col>Processo DSPSJB/{{ purchase.innerProcess }}</v-col>
+      <v-col>{{ purchase.description }}</v-col>
+      <v-col>{{ timestampToYear(purchase.date) }}</v-col>
+    </v-row>
+  </div>
   <v-alert
     v-if="error"
     type="error"
