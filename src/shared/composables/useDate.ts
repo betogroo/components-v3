90034271509b2169fb2db3e6e909a2c8004543d3
Luiz Humberto <luizhumberto@gmail.com>
2023-07-06 20:36:17 -0300

    chore: create useDate composable

---
+++ src/shared/composables/useDate.ts
@@ -0,0 +1,17 @@
+import type { Timestamp } from '@/plugins/firebase'
+
+const timestampToYear = (date: Timestamp) => {
+  const seconds = date.seconds * 1000
+  return new Date(seconds).getFullYear()
+}
+
+const timestampToDate = (date: Timestamp) => {
+  const seconds = date.seconds * 1000
+  return new Date(seconds).toLocaleDateString('pt-BR')
+}
+
+const useDate = () => {
+  return { timestampToYear, timestampToDate }
+}
+
+export default useDate
