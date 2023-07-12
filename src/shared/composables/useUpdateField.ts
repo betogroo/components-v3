738837c0fc403c0674bde1cd95b4dd954f218d3c
Luiz Humberto <luizhumberto@gmail.com>
2023-07-12 20:09:52 -0300

    chore: add updateMap and remove id parameter from useUpdateField constructor)

--- src/shared/composables/useUpdateField.ts
+++ src/shared/composables/useUpdateField.ts
@@ -1,15 +1,31 @@
-import { db, doc, updateDoc } from '@/plugins/firebase'
+import { db, doc, getDoc, updateDoc } from '@/plugins/firebase'
 import { ref } from 'vue'
 
 const formValue = ref('')
-const useUpdateField = (collection: string, id: string, field: string) => {
-  const updateField = async (value: string) => {
+const useUpdateField = (collection: string, field: string) => {
+  const updateField = async (value: string, id: string) => {
     const documentReference = doc(db, collection, id)
     await updateDoc(documentReference, { [field]: value })
   }
-  const updateMap = async <T>(value: T) => {
+  const updateMap = async <T>(value: T, id: string) => {
     const documentReference = doc(db, collection, id)
-    await updateDoc(documentReference, { [field]: value })
+    const documentSnapshot = await getDoc(documentReference)
+    let newBudget: T[] = []
+    if (documentSnapshot.exists()) {
+      const existingMap: T[] = documentSnapshot.data().budget
+
+      if (existingMap) {
+        console.log('tem budiget')
+        newBudget = [...existingMap, value]
+        await updateDoc(documentReference, { budget: newBudget })
+      } else {
+        console.log('nao tem budiget')
+        newBudget = [value]
+        await updateDoc(documentReference, { budget: newBudget })
+      }
+    } else {
+      console.log('aqui se nao existe')
+    }
   }
   return { updateField, updateMap, formValue }
 }
