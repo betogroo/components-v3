fedd7e0b13721fd65279713ad950284d8372adde
betogroo <luizhumberto@gmail.com>
2023-07-13 08:32:57 -0300

    chore: remove global style

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -13,11 +13,6 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  defaults: {
-    global: {
-      variant: 'outlined',
-    },
-  },
   theme: {
     themes: {
       light: {
