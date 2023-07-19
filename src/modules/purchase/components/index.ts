dd123e830bc0f4b20ce4700d8e5eb3a92ea35319
betogroo <luizhumberto@gmail.com>
2023-07-19 08:37:15 -0300

    refactor: add index to import shared components and composables

---
+++ src/modules/purchase/components/index.ts
@@ -0,0 +1,5 @@
+import PurchaseComponent from '../components/PurchaseComponent.vue'
+import PurchaseItems from '../components/PurchaseItems.vue'
+import PurchaseItemForm from '../components/PurchaseItemForm.vue'
+
+export { PurchaseComponent, PurchaseItemForm, PurchaseItems }
---
+++ src/shared/composables/index.ts
@@ -0,0 +1,4 @@
+import addDocument from '@/shared/composables/addDocument'
+import getCollection from '@/shared/composables/getCollection'
+
+export { addDocument, getCollection }
