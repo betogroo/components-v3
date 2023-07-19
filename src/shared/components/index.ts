5f2f28b4fa725ad8db915fd57bd4b4c160a5d552
betogroo <luizhumberto@gmail.com>
2023-07-19 08:43:08 -0300

    refactor: add new index to composable and components

---
+++ src/shared/components/index.ts
@@ -0,0 +1,6 @@
+import AppBackBtnVue from './AppBackBtn.vue'
+import AppBar from './AppBackBtn.vue'
+import AppLoader from './AppBackBtn.vue'
+import UpdateSingleField from './AppBackBtn.vue'
+
+export { AppBackBtnVue, AppBar, AppLoader, UpdateSingleField }
--- src/shared/composables/index.ts
+++ src/shared/composables/index.ts
@@ -1,4 +1,5 @@
 import addDocument from '@/shared/composables/addDocument'
 import getCollection from '@/shared/composables/getCollection'
+import useUpdateField from '@/shared/composables/useUpdateField'
 
-export { addDocument, getCollection }
+export { addDocument, getCollection, useUpdateField }
