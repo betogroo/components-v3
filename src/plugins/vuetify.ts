f7b658c49111a313614168f29caf766517e0c9b6
betogroo <luizhumberto@gmail.com>
2023-07-29 13:58:12 -0300

    chore: add global property to form items

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,8 +11,21 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+const formCommonProps = {
+  variant: 'outlined',
+  hideDetails: true,
+  density: 'compact',
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  defaults: {
+    VRow: {
+      'no-gutters': true,
+    },
+    VSelect: { ...formCommonProps },
+    VTextField: { ...formCommonProps },
+  },
   theme: {
     themes: {
       light: {
