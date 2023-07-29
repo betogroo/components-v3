aa1080ee03001d2ecb42963cae702931c1f207c5
betogroo <luizhumberto@gmail.com>
2023-07-29 13:59:30 -0300

    refactor: remove unused properties

--- src/modules/purchase/components/PurchaseItemForm.vue
+++ src/modules/purchase/components/PurchaseItemForm.vue
@@ -29,67 +29,59 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-  <v-form @submit.prevent="handleSubmit">
-    <v-row no-gutters>
-      <v-col
-        ><v-text-field
-          v-model="formValues.tittle"
-          density="compact"
-          :hide-details="true"
-          hint="Digite o nome do material, de modo amigável"
-          label="Material"
-      /></v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-text-field
-          v-model="formValues.siafisico"
-          density="compact"
-          :hide-details="true"
-          hint="Digite o número do Item Siafísico"
-          label="Item Siafísico"
-        />
-      </v-col>
-      <v-col>
-        <v-text-field
-          v-model="formValues.expenseQuality"
-          density="compact"
-          hint="Digite o número da Natureza da Despesa"
-          label="Natureza da Despesa"
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-select
-          v-model="formValues.unity"
-          clearable
-          density="compact"
-          :items="['1', '10', '408', '302', '187']"
-          label="Unidade de Fornecimento"
-        ></v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-text-field
-          v-model.number="formValues.quantity"
-          density="compact"
-          label="Quantidade"
-          type="number"
-        />
-      </v-col>
-      <v-col>
-        <v-text-field
-          v-model.number="formValues.price"
-          density="compact"
-          label="Valor Unitário"
-          type="number"
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col><v-btn type="submit">Adicionar</v-btn></v-col>
-    </v-row>
-  </v-form>
+  <v-sheet max-width="600">
+    <v-form @submit.prevent="handleSubmit">
+      <v-row>
+        <v-col class="ma-0"
+          ><v-text-field
+            v-model="formValues.tittle"
+            label="Material"
+        /></v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="formValues.siafisico"
+            label="Item Siafísico"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="formValues.expenseQuality"
+            label="Natureza da Despesa"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-select
+            v-model="formValues.unity"
+            clearable
+            :items="['1', '10', '408', '302', '187']"
+            label="Unidade de Fornecimento"
+          ></v-select>
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model.number="formValues.quantity"
+            label="Quantidade"
+            type="number"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model.number="formValues.price"
+            label="Valor Unitário"
+            type="number"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-btn type="submit">Adicionar</v-btn></v-col>
+      </v-row>
+    </v-form>
+  </v-sheet>
 </template>
