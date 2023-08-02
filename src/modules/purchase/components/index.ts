b8524a1b8953174b27148b9ac1867b36490f45b1
Luiz Humberto <luizhumberto@gmail.com>
2023-08-02 16:52:21 -0300

    refactor: delete unused components

--- src/modules/purchase/components/index.ts
+++ src/modules/purchase/components/index.ts
@@ -1,13 +1,6 @@
-import PurchaseComponent from './PurchaseComponent.vue'
 import PurchaseItems from './PurchaseItems.vue'
 import PurchaseItemForm from './PurchaseItemForm.vue'
 import PurchaseHead from './PurchaseHead.vue'
 import PurchaseDetails from './PurchaseDetails.vue'
 
-export {
-  PurchaseComponent,
-  PurchaseItemForm,
-  PurchaseItems,
-  PurchaseHead,
-  PurchaseDetails,
-}
+export { PurchaseItemForm, PurchaseItems, PurchaseHead, PurchaseDetails }
