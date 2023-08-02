bcb11a3002d6cf18d6f3e14fe0f05848944073c0
betogroo <luizhumberto@gmail.com>
2023-08-02 15:03:22 -0300

    refactor: Purchase types

--- src/modules/purchase/model/Purchase.d.ts
+++ src/modules/purchase/model/Purchase.d.ts
@@ -7,10 +7,11 @@ export interface Purchase {
   type: string
   description: string
   createdAt: Timestamp
+  purchaseItems?: PurchaseItem[]
 }
 export interface PurchaseItem {
   id?: string
-  purchase_id: string
+  purchase_id?: string
   tittle: string
   siafisico: string
   expenseQuality: string
