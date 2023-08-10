e867cf5a8f4a33a79f291cc2fce6edb7f26ef2e4
betogroo <luizhumberto@gmail.com>
2023-08-10 15:46:14 -0300

    fix: import component name

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,14 +1,14 @@
 import UpdateSingleField from './UpdateSingleField.vue'
 
 // App
-import AppBackBtnVue from './app/AppBackBtn.vue'
+import AppBackBtn from './app/AppBackBtn.vue'
 import AppLoader from './app/AppLoader.vue'
 import AppBar from './app/AppBar.vue'
 import AppNavDrawer from './app/AppNavDrawer.vue'
 import AppIconBtn from './app/AppIconBtn.vue'
 
 export {
-  AppBackBtnVue,
+  AppBackBtn,
   AppBar,
   AppLoader,
   UpdateSingleField,
