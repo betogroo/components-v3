4108fe5b5600257e37f850232bb65421b5f61405
betogroo <luizhumberto@gmail.com>
2023-08-14 14:22:34 -0300

    chore: use supabase types on supabase declare

--- src/plugins/supabase.ts
+++ src/plugins/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/shared/model'
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   'https://sxqxihgahgbcgwpcftbx.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4cXhpaGdhaGdiY2d3cGNmdGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3NzQ1MTcsImV4cCI6MjAwNzM1MDUxN30.H5uKzQjQ2zPx_K9qd7BkM90ECQzS2_jX1a5DdSi3_QA',
 )
