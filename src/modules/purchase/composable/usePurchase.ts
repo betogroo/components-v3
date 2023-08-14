d6d2d163d51a9696d11beb7aa650c394fe257fb4
betogroo <luizhumberto@gmail.com>
2023-08-14 16:26:33 -0300

    refactor[PurchaseHome]: changed firebase to supabase

--- src/modules/purchase/composable/usePurchase.ts
+++ src/modules/purchase/composable/usePurchase.ts
@@ -1,18 +1,49 @@
+// vue
+import { ref } from 'vue'
 // composables
-import useDate from '@/shared/composables/useDate'
+import { useDate } from '@/shared/composables'
+import { supabase } from '@/plugins/supabase'
 
 //types
 import type { Purchase } from '../model/'
-import { Timestamp } from '@/shared/model'
+
 const { timestampToDate, timestampToYear } = useDate()
+const purchases = ref<Purchase[]>([])
+const isLoading = ref(false)
+const error = ref()
+const purchaseCount = ref<number | null>(0)
+
 const usePurchase = () => {
+  const getData = async () => {
+    isLoading.value = true
+    try {
+      const {
+        data,
+        error: err,
+        count,
+      } = await supabase.from('purchase').select('*').order('innerProcess')
+      if (data) {
+        isLoading.value = false
+        purchases.value = data
+        purchaseCount.value = count
+      } else {
+        isLoading.value = false
+        error.value = err
+      }
+    } catch (err) {
+      isLoading.value = false
+      error.value = err
+    }
+  }
+
   const innerProcessFormatName = (purchase: Purchase, prefix = 'DSP SJB') => {
-    const innerProcess = purchase.innerProcess.toString().padStart(3, '0')
-    const year = timestampToYear(purchase.createdAt)
+    const innerProcess = purchase.innerProcess?.toString().padStart(3, '0')
+    //const year = timestampToYear(purchase.createdAt)
+    const year = new Date(purchase.createdAt).getFullYear()
     return `Processo: ${prefix}/${innerProcess}/${year}`
   }
 
-  const dateFormat = (date: Timestamp) => {
+  const dateFormat = (date: string) => {
     return timestampToDate(date)
   }
   const itemsCount = (
@@ -34,7 +65,16 @@ const usePurchase = () => {
     }
     return text
   }
-  return { innerProcessFormatName, dateFormat, itemsCount }
+  return {
+    innerProcessFormatName,
+    dateFormat,
+    itemsCount,
+    getData,
+    purchases,
+    purchaseCount,
+    error,
+    isLoading,
+  }
 }
 
 export default usePurchase
