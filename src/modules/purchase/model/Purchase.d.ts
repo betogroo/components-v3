bd0cacee3f01ee1f0b66bb25dafdf80049b528dc
betogroo <luizhumberto@gmail.com>
2023-08-14 14:45:21 -0300

    chore: adapt type sto supabase

--- src/modules/purchase/model/Purchase.d.ts
+++ src/modules/purchase/model/Purchase.d.ts
@@ -1,15 +1,26 @@
-import { Timestamp } from '@/shared/model'
+//import { Timestamp } from '@/shared/model'
+import type { Database } from '@/shared/model'
 
-export interface Purchase {
+/* export interface Purchase {
   id?: string
   innerProcess: number
   outerProcess?: string
-  type: string
   description: string
-  createdAt: Timestamp
-  purchaseItems?: PurchaseItem[]
-}
-export interface PurchaseItem {
+  createdAt: Date
+  purchaseTypeId: PurchaseType.id
+} */
+
+type Purchase = Database['public']['Tables']['purchase']['Row']
+type PurchaseType = Database['public']['Tables']['purchase_type']['Row']
+type PurchaseItem = Database['public']['Tables']['purchase_item']['Row']
+type PurchaseHasItem =
+  Database['public']['Tables']['purchase_purchase_item']['Row']
+
+/* export interface PurchaseType {
+  id: string
+  title: string
+} */
+/* export interface PurchaseItem {
   id?: string
   tittle: string
   siafisico: string
@@ -19,7 +30,7 @@ export interface PurchaseItem {
   price: number
   createdAt: Timestamp
   budgets?: Budget[]
-}
+} */
 
 export interface Budget {
   link?: string
