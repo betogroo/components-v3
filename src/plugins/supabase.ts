4448fddbe6d70e38a72a34810163ac691ef9a654
betogroo <luizhumberto@gmail.com>
2023-08-24 11:30:26 -0300

    chore: #49 create env.local to supabase credentials

--- src/plugins/supabase.ts
+++ src/plugins/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/shared/model'
-
-export const supabase = createClient<Database>(
-  'https://sxqxihgahgbcgwpcftbx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4cXhpaGdhaGdiY2d3cGNmdGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3NzQ1MTcsImV4cCI6MjAwNzM1MDUxN30.H5uKzQjQ2zPx_K9qd7BkM90ECQzS2_jX1a5DdSi3_QA',
-)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
